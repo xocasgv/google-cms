db7f0263ee5c5cae83564a25533023f65d53837a
tanatos38@gmail.com <tanatos38@gmail.com@4939d56e-29e9-2154-7cbd-aad660bdbc6e>
2011-07-24 20:29:27 +0000

    remove of  and add some comments for the paths

--- trunk/config.php
+++ trunk/config.php
@@ -2,10 +2,9 @@
 $login = "your.google.login@gmail.com";
 $password = "YouR!Pa55WoRD";
 $rootFolderId = "0B4JJj8emNjB3OTc0Mjg1YWMtMzViZC00NjczLThjNmUtOWU1MTRkNDJkYjI3"; // Look at your Google Doc root folder URL
-$headerPath = "../google-cms/header.php";
-$footerPath = "../google-cms/footer.php";
-$rootPath = "..";
+$headerPath = "../google-cms/header.php"; // Path from a page.php to the header
+$footerPath = "../google-cms/footer.php"; // Path from a page.php to the footer
+$rootPath = ".."; // Path from googlecms.php to the root of the website
 $folderMode = 0755; // Requested by provider (ovh)
 $fileMode = 0604; // Requested by provider (ovh)
-$style = False; // Use Google Doc styles if True
 ?>
