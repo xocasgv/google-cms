67e72026adef80dc61d46884c4b612e415470186
tanatos38@gmail.com <tanatos38@gmail.com@4939d56e-29e9-2154-7cbd-aad660bdbc6e>
2011-09-10 17:13:01 +0000

    Update example/config.php

--- trunk/example/config.php
+++ trunk/example/config.php
@@ -1,10 +1,11 @@
 ﻿<?php
-$login = "your.google.login@gmail.com";
-$password = "YouR!Pa55WoRD";
-$rootFolderId = "0B4JJj8emNjB3OTc0Mjg1YWMtMzViZC00NjczLThjNmUtOWU1MTRkNDJkYjI3"; // Look at your Google Doc root folder URL
-$headerPath = "../google-cms/header.php"; // Path from a page.php to the header
-$footerPath = "../google-cms/footer.php"; // Path from a page.php to the footer
-$rootPath = ".."; // Path from googlecms.php to the root of the website
+$login = "gt2901351@gmail.com";
+$password = "gt2901351gt2901351";
+$rootFolderId = "0B0mgIgAsLI9qODA4NWU3ZjItZWE5Yi00MGFlLTg0NzItNzE4YTVhNjZhNjBh";
+// Look at your Google Doc root folder URL
+$headerPath = "header.php"; // Path from a page.php to the header
+$footerPath = "footer.php"; // Path from a page.php to the footer
+$rootPath = ""; // Path from googlecms.php to the root of the website
 $folderMode = 0755; // Requested by provider (ovh)
 $fileMode = 0604; // Requested by provider (ovh)
 ?>
