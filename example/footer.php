6b2fc5e2d2961da440050b964c606c0afdfa3879
Tanatos38 <tanatos38@gmail.com>
2011-09-10 16:53:18 +0000

    Set up the example html page

---
+++ example/footer.php
@@ -0,0 +1,8 @@
+<!-- ------------------------------------------------ --> 
+		</div> <!-- corpus --> 
+	</div> <!-- page --> 
+	<div id="footer">
+		There is nothing to say in this footer.
+	</div>
+</body> 
+</html>
