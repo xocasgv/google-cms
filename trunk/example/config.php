750805991a8cdcb03193d590fa4f0b15c3ad4110
Tanatos38@gmail.com <Tanatos38@gmail.com@4939d56e-29e9-2154-7cbd-aad660bdbc6e>
2012-06-18 21:32:42 +0000

    Merged code is fully functional. Pages now flush befor checking for updades.

--- trunk/example/config.php
+++ trunk/example/config.php
@@ -1,10 +1,20 @@
-﻿<?php
-$login = "gt2901351@gmail.com";
-$password = "gt2901351gt2901351";
-$rootFolderId = "0B0mgIgAsLI9qODA4NWU3ZjItZWE5Yi00MGFlLTg0NzItNzE4YTVhNjZhNjBh";
-// Root folder id Google Doc root folder URL
-$templatePath = "template.php"; // Path from a page.php to the template.php file
-$rootPath = ""; // Path from googlecms.php to the root of the website
-$folderMode = 0755;
-$fileMode = 0604;
+<?php
+define("LOGIN", "gt2901351@gmail.com");
+define("PASSWORD", "gt2901351gt2901351");
+define("ROOT_FOLDER_ID", "0B0mgIgAsLI9qODA4NWU3ZjItZWE5Yi00MGFlLTg0NzItNzE4YTVhNjZhNjBh");
+
+define("ROOT_PATH", "");
+define("LAST_UPDATE_DATE", "2012-06-18T20:28:57");
+define("LAST_AUTH_STRING", "DQAAAKoAAABpshXRLy6fkbRyM4ToY6VwAd9KK0r2GkjnC2UaanyG6kj-7x53eyELFefPqacEE55UwzWeNf3pdKUhT6RtbaLJ2L_JP9QFt1dSn6PRQb_EIeFgbp7yXzyyiEoDVlwxMSSTEvJ_72V62B4k0hGk3nO4J2o93vCREXG0IHJHAw93AJZU72vTCT5DbrfW4ytmkY0VlK7_SfmCUXAf4rygGay4FCI2mpSfe7ZyzbtvXSVnmQ");
+$MENU = Array(
+    "folders" => Array
+        (
+        ),
+    "pages" => Array
+        (
+            "0" => "Index",
+            "1" => "Consectetur",
+            "2" => "Page 3",
+        ),
+);
 ?>
